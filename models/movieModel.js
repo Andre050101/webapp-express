55dee3638f4ca0df72d9f50b6f0dedfe664720ff
Andrea050101 <agiancristiano90@gmail.com>
2025-01-30 15:55:57 +0100

    queries function moved in models/movieModel.js

---
+++ models/movieModel.js
@@ -0,0 +1,19 @@
+import { db } from "../config/db.js";
+
+export const getAllMovies = async () => {
+    const query = "SELECT * FROM movies";
+    const [results] = await db.query(query);
+    return results;
+};
+
+export const getMovieById = async (id) => {
+    const query = "SELECT * FROM movies WHERE id = ?";
+    const [results] = await db.promise().query(query, [id]);
+    return results[0];
+}
+
+export const getReviewsByMovieId = async (movieId) => {
+    const query = "SELECT * FROM reviews WHERE movie_id = ?";
+    const [results] = await db.promise().query(query, [movieId]);
+    return results;
+};
